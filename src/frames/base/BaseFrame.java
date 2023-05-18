d8db3c453feb7773c5700f0744f1eff82bd69219
adambuckley12 <adam@brownthwaite.com>
2023-05-18 22:24:41 +0100

    added Base frame comments

--- src/frames/base/BaseFrame.java
+++ src/frames/base/BaseFrame.java
@@ -10,16 +10,17 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.io.File;
 
-
 public abstract class BaseFrame extends JFrame {
     protected BasePanel parentPanel;
 
-
+    // Abstract method for custom painting
     public abstract void customPaint(Graphics gr, int width, int height);
 
+    // Abstract method for updating component positions
     public abstract void updatePositions();
 
     /**
+     * Plays a sound based on the provided sound name.
      * Sound options:
      * src/assets/audio/applause.wav
      * src/assets/audio/bounce.wav
@@ -30,19 +31,21 @@ public abstract class BaseFrame extends JFrame {
      * @param soundName The path of the sound file to play
      */
     public void playSound(String soundName) {
+        // Check if audio is enabled in the parent panel
         if (!parentPanel.audioEnabled) return;
         try {
+            // Load the sound file and play it using the Clip class
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             clip.start();
-
         } catch (Exception ex) {
+            // Print an error message if there is an issue playing the sound
             System.out.println("Error with playing sound.");
             ex.printStackTrace();
         }
     }
 
+    // Abstract method for handling mouse events
     public abstract void MouseEvent(MouseEvent e);
-
 }
