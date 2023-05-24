e865bc8f81d10e2c448211cef01a5bd4a15f038f
Adam Buckley <9486588+adambuckley12@users.noreply.github.com>
2023-05-24 09:12:21 +0100

    increase friction to be noticable

--- src/panels/base/BasePanel.java
+++ src/panels/base/BasePanel.java
@@ -18,7 +18,7 @@ public abstract class BasePanel extends JPanel implements Runnable, KeyListener,
     public double playerSpeed = 6;
     public int winner = 0;
     public float coefRestitution = 0.9f;
-    public float coefFriction = 0.0025f;
+    public float coefFriction = 0.008f;
 
     protected int width;
     protected int height;
